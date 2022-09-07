711a84922da7200dbdc4836a9d7e1516602d22a6
TooIneptNeedToLearn <113023885+TooIneptNeedToLearn@users.noreply.github.com>
2022-09-07 22:28:49 +0800

    Add files via upload

---
+++ PythagoreanTheoremPrac/PythagoreanTheoremPrac/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       //Pythagorean Theorem
+        int a, b;
+        double a1,b1,c,c1;
+        a = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("You've selected: "+a);
+        b = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("You've selected: " + b);
+
+        a1 = Math.Sqrt(a);
+        Console.WriteLine("The sqrt of a: "+a1);
+
+        b1 = Math.Sqrt(b);
+        Console.WriteLine("The sqrt of b: "+b1);
+
+        c = a1 + b1;
+        c1 = Math.Sqrt(c);
+        Console.WriteLine("Your constant is "+c);
+        Console.WriteLine("Your hypotenuse is "+c1);
+
+
+    }
+}

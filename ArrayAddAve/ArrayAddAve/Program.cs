c90aeae9bac08ddf04359c2389bb4dd4fc5ccbfa
TooIneptNeedToLearn <113023885+TooIneptNeedToLearn@users.noreply.github.com>
2022-09-09 18:08:57 +0800

    Add files via upload

---
+++ ArrayAddAve/ArrayAddAve/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] number = {1,2,3,4,5,6,7,8,9,10};
+        int sum, average;
+        sum = 0;
+
+        for (int i = 0; i < number.Length; i++)
+        {
+            Console.WriteLine(number[i]);
+            sum+= number[i];
+        }
+        //Print the sum of the elements
+        Console.WriteLine("------------------");
+        Console.WriteLine("The sum is "+sum);
+        average = sum/number.Length;
+        Console.WriteLine("You're average is "+average);
+    }    
+
+
+}
